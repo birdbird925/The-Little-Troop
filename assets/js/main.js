b30e6e62b6a84fb2dff6b801704b1e1daf85b387
xiangwen94 <xiangwen94@gmail.com>
2017-02-07 15:06:50 +0800

    intro page image gif effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,16 @@ $(function() {
     $('.menu-tab').click(function() {
       $('.wrapper').toggleClass('reveal-nav');
     });
+
+    //intro page image gif effect
+    var layer = 1;
+    setInterval(function(){
+      $('.layer:eq('+layer+')').css("visibility", "visible");
+      layer++;
+
+      if(layer == 17) {
+        layer = 1;
+        $('.layer:gt(0)').css("visibility", "hidden");
+      }
+    }, 500);
 });
